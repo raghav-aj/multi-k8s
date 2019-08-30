cc6f524573fbd485f2393c6273e647bbe2422134
raghav.nayak <raghavendra.nayak@aasaanjobs.com>
2019-08-30 18:20:17 +0530

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+# building the images
+docker build -f raghavn-aasaanjobs/multi-client:latest -t raghavn-aasaanjobs/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -f raghavn-aasaanjobs/multi-server:latest -t raghavn-aasaanjobs.multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -f raghavn-aasaanjobs/multi-worker:latest -t raghavn-aasaanjobs.multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# we have already logged in to Docker hub through travis. so, no need to login again.
+# pushing the images to docker hub
+docker push raghavn-aasaanjobs/multi-client:latest
+docker push raghavn-aasaanjobs/multi-client:$SHA
+
+docker push raghavn-aasaanjobs/multi-server:latest
+docker push raghavn-aasaanjobs/multi-server:$SHA
+
+docker push raghavn-aasaanjobs/multi-worker:latest
+docker push raghavn-aasaanjobs/multi-worker:$SHA
+
+# running the kubernetes
+kutectl apply -f k8s
+
+# imperatively set latest images on each deployment
+# server-deployment -> the name given in k8s/server-deployment.yaml
+# server= -> the name of the server container in k8s/server-deployment.yaml
+kubectl set image deployments/server-deployment server=raghavn-aasaanjobs/multi-server:$SHA
+kubectl set image deployments/client-deployment client=raghavn-aasaanjobs/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=raghavn-aasaanjobs/multi-worker:$SHA
+
