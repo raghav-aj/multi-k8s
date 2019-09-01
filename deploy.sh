9c47ac4d4a988f6d9169c481ce7821a1b4129d53
raghav.nayak <raghavendra.nayak@aasaanjobs.com>
2019-09-01 20:05:27 +0530

    modified the docker username in the travis and deploy files

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 # building the images
-docker build -t raghavn-aasaanjobs/multi-client:latest -t raghavn-aasaanjobs/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t raghavn-aasaanjobs/multi-server:latest -t raghavn-aasaanjobs.multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t raghavn-aasaanjobs/multi-worker:latest -t raghavn-aasaanjobs.multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t raghavnayakaasaanjobs/multi-client:latest -t raghavnayakaasaanjobs/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t raghavnayakaasaanjobs/multi-server:latest -t raghavnayakaasaanjobs/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t raghavnayakaasaanjobs/multi-worker:latest -t raghavnayakaasaanjobs/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 # we have already logged in to Docker hub through travis. so, no need to login again.
 # pushing the images to docker hub
-docker push raghavn-aasaanjobs/multi-client:latest
-docker push raghavn-aasaanjobs/multi-client:$SHA
+docker push raghavnayakaasaanjobs/multi-client:latest
+docker push raghavnayakaasaanjobs/multi-client:$SHA
 
-docker push raghavn-aasaanjobs/multi-server:latest
-docker push raghavn-aasaanjobs/multi-server:$SHA
+docker push raghavnayakaasaanjobs/multi-server:latest
+docker push raghavnayakaasaanjobs/multi-server:$SHA
 
-docker push raghavn-aasaanjobs/multi-worker:latest
-docker push raghavn-aasaanjobs/multi-worker:$SHA
+docker push raghavnayakaasaanjobs/multi-worker:latest
+docker push raghavnayakaasaanjobs/multi-worker:$SHA
 
 # running the kubernetes
 kutectl apply -f k8s
@@ -20,7 +20,7 @@ kutectl apply -f k8s
 # imperatively set latest images on each deployment
 # server-deployment -> the name given in k8s/server-deployment.yaml
 # server= -> the name of the server container in k8s/server-deployment.yaml
-kubectl set image deployments/server-deployment server=raghavn-aasaanjobs/multi-server:$SHA
-kubectl set image deployments/client-deployment client=raghavn-aasaanjobs/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=raghavn-aasaanjobs/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=raghavnayakaasaanjobs/multi-server:$SHA
+kubectl set image deployments/client-deployment client=raghavnayakaasaanjobs/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=raghavnayakaasaanjobs/multi-worker:$SHA
 
